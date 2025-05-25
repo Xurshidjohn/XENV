2f13274151b0b5732cb433e8738b60078271a2b6
Xurshidjohn <xurshidjonxurshid45@gmail.com>
2025-05-25 09:56:54 +0500

    xatolik bor! 67-lineda nullarni yoqotish kerak hozircha boshqa xatolik yo'q!

--- index.c
+++ index.c
@@ -1,44 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 const char *text = "[font] = `Menlo`;[salom] = `salom`";
 const char words[100];
 
-typedef enum TokenType {
-	BRACK_L = 0,
-	LETTER = 1,
-	BRACK_R = 2,
-	EQUAL = 3,
-	QUOTE = 4,
-	COLON = 5,
-	BACK_TAG = 6,
-	SPACE = 7
-} t_type;
+typedef enum TokenType {BRACK_L = 0,LETTER = 1,BRACK_R = 2,
+EQUAL = 3,QUOTE = 4,COLON = 5,BACK_TAG = 6,SPACE = 7} t_type;
+typedef enum KEYWORD{KEY = 0,VALUE = 1} KEYWORD;
+typedef struct Token {t_type type;char value;} t_token;
+typedef struct Token_v2 {KEYWORD type;char *value;} token_v2;
 
-typedef struct Token {
-	t_type type;
-	char value;
-} t_token;
-
-t_token tokens[200] = {};
-int i_token = 0;
+t_token tokens[200];
+token_v2 v2_tokens[200];
+int i_token = 0, v2_token_i = 0;
 void display_token(t_token token)
 {
 	printf("<Token value='%c' type='%d'>\n", token.value, token.type);
 }
 
 char *get_keys() {
- 	int flag = 0;
- 	char *key = malloc(100 * sizeof(char*));
- 	int key_i = 0;
+int flag = 0;
+ 	char *key = malloc(100 * sizeof(char*));int key_i = 0;
 	for(int i = 0;i < 200;i++) {
 		if(tokens[i].value == '[') {
 			flag = 1;
 		}
 		if(tokens[i-1].value == ']') {
-			printf("%s\n", key);
-			flag = 0;
-			key_i = 0;
+			struct Token_v2 token;
+			token.type =1;
+			token.value = malloc(100 * sizeof(char*));
+			strcpy(token.value, key);
+			v2_tokens[v2_token_i] = token;
+			v2_token_i++;key_i=0;key[0]='\0';flag=0;
 		}
 		if(flag == 1) {
 			key[key_i] = tokens[i].value;
@@ -68,4 +62,9 @@ void tokenizer(char *text)
 int main() {
 	tokenizer(text);
 	get_keys();
+	for(int i = 0;i < 10;i++) {
+		if(v2_tokens[i].type == 1 || v2_tokens[i].type == 0) {
+			printf("%s", v2_tokens[i].value); // ERROR BOR MANA SHU QATORDA NAXUY
+		}
+	}
 }
